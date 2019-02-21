db52bce6418a178176487f422bf85fc948c3cef2
Franco Canevali <francocanevali@gmail.com>
2019-02-21 12:47:00 -0300

    clean

---
+++ v2/MyGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreLibs
+{
+    public class MyGeneric<T,U> where T : class, new() where U : struct
+    {
+        T key;
+        U value;
+        Dictionary<T, U> diccionario;
+
+        public MyGeneric()
+        {
+            diccionario = new Dictionary<T, U>();
+        }
+
+        //MyGeneric<StringBuilder, int> gen = new MyGeneric<StringBuilder, int>();
+    }
+}
